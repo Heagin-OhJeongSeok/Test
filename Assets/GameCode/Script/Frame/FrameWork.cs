0c076e69e2fbfed2beb168d754c87cbd6bb706c9
오정석 <wjdtjr0114@haegin.kr>
2023-03-28 23:53:39 +0900

    [1.59.0][오정석] #XXXX / [클라] 1. FrameWork 스크립트 오류 수정 2. 광장 맵 건물 추가 3. DialogEventHome 프리팹 수정

--- Assets/GameCode/Script/Frame/FrameWork.cs
+++ Assets/GameCode/Script/Frame/FrameWork.cs
@@ -7,7 +7,7 @@ public class FrameWork : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Test
     }
 
     // Update is called once per frame
